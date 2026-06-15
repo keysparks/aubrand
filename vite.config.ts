3ae6b0079e1f88d83930499974dc8bba07c4b7cc
Saravanan GR <83922152+keysparks@users.noreply.github.com>
2026-06-15 19:00:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/aubrand/',
+})
 
 function figmaAssetResolver() {
   return {
